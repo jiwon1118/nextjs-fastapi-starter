58333099ab8ca240cbd7a3ee0ecb6f18921cb84e
jiwon1118 <b23386585@gmail.com>
2025-01-08 23:09:14 +0900

    코드 수정

--- api/index.py
+++ api/index.py
@@ -18,8 +18,18 @@ def age_calculator(birthday: str) -> Dict[str, str]:
     :param birthday: 생년월일 (형식: YYYY-MM-DD)
     :return: 생년월일 및 만나이를 포함한 JSON 응답
     """
-    random_age = random.randint(0, 100)
+    #random_age = random.randint(0, 100)
+    
     today = date.today()
+    birth_date = datetime.strptime(birthday, "%Y-%m-%d").date()
+
+
+    # TODO 생일 지난 여부 관련 로직 코드 작성
+    if birth_date.month < today.month or (birth_date.month == today.month and birth_date.day <= today.day):
+        age = today.year - birth_date.year
+    else:
+        age = today.year - birth_date.year - 1
+
     
     return {
             "birthday": birthday,
