6fa2b8202523d1cfec85324e390054440746ecb5
jiwon1118 <b23386585@gmail.com>
2025-01-10 09:28:59 +0900

    리턴 생성

--- api/index.py
+++ api/index.py
@@ -46,6 +46,7 @@ def age_calculator(birthday: str) -> Dict[str, str]:
     return {
             "birthday": birthday,
             "age": str(age),
+            "zodiac": str(zodiac)
             "basedate": str(today),
             "message": "Age calculated successfully!"
             }
