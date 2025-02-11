ae72d256694d114938b0855ed44f570a1761dadd
jiwon1118 <b23386585@gmail.com>
2025-02-11 15:40:53 +0900

    view

--- api/index.py
+++ api/index.py
@@ -70,23 +70,7 @@ DB_CONFIG = {
 
 @app.get("/api/py/select_all")
 def select_table():
-    query = """
-    SELECT
-        l.menu_name,
-        m.name,
-        l.dt
-    FROM
-        lunch_menu l
-        inner join member m
-        on l.member_id = m.id
-    ORDER BY
-        dt DESC;
-    ;
-    """
     with psycopg.connect(**DB_CONFIG, row_factory=dict_row) as conn:
-        cur = conn.execute(query)
+        cur = conn.execute("SELECT * FROM view_select_all")
         rows = cur.fetchall()
         return rows
-
-    #df = pd.DataFrame(rows, columns=['menu_name','member_name','dt'])
-    #return df
